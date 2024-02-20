ac78357bc4d39396e9bd0c36eaf5b1f2d3b644be
Rashmi <rathnakumarrashmi2@gmail.com>
2024-02-20 22:30:30 +0530

    setting up backend

---
+++ src/main/java/com/example/employems/EmployemsApplication.java
@@ -0,0 +1,13 @@
+package com.example.employems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployemsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployemsApplication.class, args);
+	}
+
+}
